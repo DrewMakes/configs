598d2e603e4129c3862cccf4dca12d4e06ae6d7a
Drew Bradford <hey@drewmakes.com>
2020-03-30 15:39:40 +0000

    added config alias comments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 set PATH $HOME/.cargo/bin $PATH
 
+# use 'config' instead of 'git' when adding dotfiles to github
+# $ config add filename.conf
+# $ config commit -m '<message>'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
